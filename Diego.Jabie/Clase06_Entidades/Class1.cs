0e62e1abf7f0153482cd5fad1c9d36b5c104c82e
DiegoJabie1 <47386362+DiegoJabie1@users.noreply.github.com>
2019-09-04 11:45:44 -0300

    Add files via upload

---
+++ Diego.Jabie/Clase06_Entidades/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Clase06_Entidades
+{
+    public class Temperas
+    {
+        private ConsoleColor color;
+        private string marca;
+        private int cantidad;
+
+        public Temperas(ConsoleColor color, string marca, int cantidad)
+        {
+            this.color = color;
+            this.marca = marca;
+            this.cantidad = cantidad;
+
+        }
+
+        private string Mostrar()
+        {
+            return this.color.ToString() + " " + this.marca.ToString() + " " + this.cantidad.ToString();
+        }
+
+        public static implicit operator string(Temperas tempera)
+        {
+            return tempera.Mostrar();
+        }
+
+        public static bool operator ==(Temperas tempera1, Temperas tempera2)
+        {
+            bool retorno = false;
+
+            if (Object.Equals(tempera1, null) == false && Object.Equals(tempera2, null) == false)
+            {
+                if (tempera1.marca == tempera2.marca && tempera1.color == tempera2.color)
+                {
+                    retorno = true;
+                }
+     
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Temperas tempera1,Temperas tempera2)
+        {
+            return !(tempera1 == tempera2);
+        }
+
+        public static Temperas operator +(Temperas tempera, int num)
+        {
+            if (Object.Equals(tempera, null) == false)
+            {
+                tempera.cantidad = tempera.cantidad + num;
+            }
+            return tempera;
+        }
+
+        public static Temperas operator +(Temperas tempera1, Temperas tempera2)
+        {
+            if (tempera1 == tempera2)
+            {
+                tempera1 += tempera2.cantidad;
+            }
+
+            return tempera1;
+        }
+    }
+}
---
+++ Diego.Jabie/Clase06_Entidades/Class2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Clase06_Entidades;
+
+namespace Clase_06_Entidades
+{
+    class Paletas
+    {
+        private Temperas[] colores;
+        private int cantidadMaximaColores;
+
+        public Paletas() : this(5)
+        {
+        }
+
+        public Paletas(int num)
+        {
+            this.colores = new Temperas[num];
+            this.cantidadMaximaColores = num;
+        }
+
+        public static implicit operator Paletas(int num)
+        {
+            return new Paletas(num);
+        }
+
+        private string Mostrar()
+        {
+            string retorno = "Cantidad maxima colores: " + this.cantidadMaximaColores.ToString() + "\n";
+
+            foreach (Temperas colores in this.colores)
+            {
+                retorno += (string)colores+"\n";
+            }
+
+            return retorno;
+        }
+
+        public static bool operator ==(Paletas paleta, Temperas tempera)
+        {
+            bool retorno = false;
+            foreach(Temperas actual in paleta.colores)
+            {
+                if (actual == tempera)
+                {
+                    retorno = true;
+                }
+            }
+        
+                
+        return retorno;        
+        }
+       
+        public static bool operator !=(Paletas paleta, Temperas tempera)
+        {
+            return !(paleta == tempera);
+        }
+
+        private int ObtenerLugarLibre()
+        {
+            int index = -1;
+
+            for (int i=0; i < this.colores.Length; i++)
+            {
+                if (this.colores[i] == null)
+                {
+                    index = i;
+
+                    break;
+                }
+
+            }
+            return index;
+        }
+
+        public static Paletas operator +(Paletas paleta, Temperas tempera)
+        {
+            if (paleta == tempera)
+            {
+                int index;
+                index = Array.IndexOf(paleta.colores, tempera);
+
+                paleta.colores[index] = paleta.colores[index] + tempera;
+            }
+
+            else if(paleta.ObtenerLugarLibre()!= -1)
+            {
+                int index = paleta.ObtenerLugarLibre();
+
+                paleta.colores[index] = tempera;
+            }
+
+            return paleta;
+        }
+
+    }
+}
